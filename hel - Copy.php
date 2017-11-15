47e57fc93028cd1e9022fd457be0200145d82723
A <you@example.com>
2017-11-15 17:12:15 +0200

    aa

---
+++ hel - Copy.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+    border-collapse: collapse;
+    width: 100%;
+}
+
+th, td {
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even){background-color: #91e842;
+  color: grey;
+
+
+
+}
+tr:nth-child(odd){background-color: #91e842;
+  color: grey;
+}
+
+
+th {
+    background-color: #4e590e;
+    color: white;
+}
+</style>
+</head>
+<body>
+
+
+
+<table>
+  <tr>
+    <th>Pavadinimas</th>
+    <th>Veiksmas</th>
+
+  </tr>
+
+
+
+  <tr>
+    <td colspan="2">Pavadinimas</td>
+    <td> Veiksmas </td>
+  </tr>
+  <tr>
+    <td colspan="3">Vaisiai</td>
+  </tr>
+  <tr>
+    <td>Obuolys</td>
+  <td><img src="http://paneveziorvsb.lt/wp-content/uploads/2016/06/abuolys-vietoj-cigaretes.png" alt="morka" height="42" width="42"></a> </td>
+    <td> <button type="button">Pirkti</button> </td>
+  </tr>
+  <tr>
+    <td>Apelsinas</td>
+    <td><img src="http://www.smagiosvestuves.lt/wp-content/uploads/2015/02/apelsinas.jpg" alt="morka" height="42" width="42"></a> </td>
+    <td> <button type="button"></button> </td>
+</tr>
+
+
+<tr>
+<td colspan="3">Daržovės</td>
+</tr>
+
+<tr>
+  <td>Morka</td>
+  <td><img src="http://edieta.mama.lt/images/default/source/Darzoves/morkos_5.jpg" alt="morka" height="42" width="42"></a> </td>
+  <td> <button type="button"></button> </td>
+
+</tr>
+
+
+
+</table>
+
+</body>
+</html>
---
+++ hel.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, th, td{ border:1px solid black; }
+</style>
+</head>
+<body>
+<table>
+		<tr>
+			<td colspan="2">Pavadinimas</td>
+			<td>Veiksmas</td>
+		</tr>	
+		<tr>
+			<td colspan="3">Vaisiai</td>
+		</tr>
+		<tr>
+			<td>Obuolys</td>
+			<td><img src="http://paneveziorvsb.lt/wp-content/uploads/2016/06/abuolys-vietoj-cigaretes.png" height=40 width="40"></td>
+			<td><button type ="button">Pirkti</button></td>
+		</tr>
+		<tr>
+			<td>Apelsinas</td>
+			<td><img src="http://www.smagiosvestuves.lt/wp-content/uploads/2015/02/apelsinas.jpg" height="40" width="40"></td>
+			<td><button type="button"></button></td>
+		</tr>	
+		<tr>
+			<td colspan="3">Daržovės</td>
+		</tr>
+		<tr>
+			<td>Morka</td>
+			<td><img src="http://edieta.mama.lt/images/default/source/Darzoves/morkos_5.jpg" height="40" width="40"></td>
+			<td><button type="button"></button></td>
+		</tr>
+	        </table>
+			</body>
+			</html>
